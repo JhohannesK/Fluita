207ad4e713f35292fc0651af1538bce9e6223121
JhohannesK <Jdomeh77@gmail.com>
2022-10-06 14:21:05 +0000

    add icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,32 +2,35 @@ import Image from 'next/image';
 import React from 'react';
 import SideIconAndText from './_sideIcon';
 // HACK: Install @types/heroicons/react
-// import {
-// 	BellIcon,
-// 	HashtagIcon,
-// 	BookmarkIcon,
-// 	CollectionIcon,
-// 	DotsCircleHorizontalIcon,
-// 	MailIcon,
-// 	HomeIcon,
-// } from '@heroicons/react/outline';
+import {
+	BellIcon,
+	HashtagIcon,
+	BookmarkIcon,
+	MagnifyingGlassIcon,
+	HomeIcon,
+	UserIcon,
+	CogIcon,
+	EnvelopeIcon,
+	EllipsisHorizontalCircleIcon,
+} from '@heroicons/react/24/outline';
+import { BsTwitter } from 'react-icons/bs';
 
 const Sidebar = () => {
 	return (
 		<div className='flex flex-col'>
 			{/* Logo for Fluita */}
-			<Image height={20} width={10} src='/images/insta.png' alt='logo' />
+			<BsTwitter size={30} className='text-mainCol' />
 
 			{/* Icon and text for the rows on the sidebar */}
 			{/* TODO: Add icons as props when they are installed */}
-			<SideIconAndText title='Home' />
-			<SideIconAndText title='Explore' />
-			<SideIconAndText title='Search' />
-			<SideIconAndText title='Profile' />
-			<SideIconAndText title='Notification' />
-			<SideIconAndText title='Messages' />
-			<SideIconAndText title='Settings' />
-			<SideIconAndText title='More' />
+			<SideIconAndText Icon={HomeIcon} title='Home' />
+			<SideIconAndText Icon={HashtagIcon} title='Explore' />
+			<SideIconAndText Icon={MagnifyingGlassIcon} title='Search' />
+			<SideIconAndText Icon={UserIcon} title='Profile' />
+			<SideIconAndText Icon={BellIcon} title='Notification' />
+			<SideIconAndText Icon={EnvelopeIcon} title='Messages' />
+			<SideIconAndText Icon={CogIcon} title='Settings' />
+			<SideIconAndText Icon={EllipsisHorizontalCircleIcon} title='More' />
 		</div>
 	);
 };
--- components/_sideIcon.tsx
+++ components/_sideIcon.tsx
@@ -2,13 +2,14 @@ import React, { SVGProps } from 'react';
 
 interface Props {
 	// TODO: Remove optional props when icons are made available.
-	Icon?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+	Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
 	title: string;
 }
 
 const SideIconAndText = ({ Icon, title }: Props) => {
 	return (
-		<div className='flex max-w-[10rem] items-center space-x-2 px-2 py-4 hover:bg-gray-100 cursor-pointer rounded-full transition-all duration-300 group'>
+		<div className='flex max-w-[10rem] items-center space-x-2 px-3 py-4 hover:bg-gray-100 cursor-pointer rounded-full transition-all duration-300 group'>
+			<Icon className='h-8 w-8 group-hover:text-blue-500' />
 			<p className='group-hover:text-mainCol'>{title}</p>
 		</div>
 	);
